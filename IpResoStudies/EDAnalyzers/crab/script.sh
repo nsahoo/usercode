820a743ac228234729d87772704e82579f30d61c
mangano <>
2010-05-01 15:29:59 +0000

    add new options for respAndReso tasks

--- IpResoStudies/EDAnalyzers/crab/script.sh
+++ IpResoStudies/EDAnalyzers/crab/script.sh
@@ -16,9 +16,25 @@ case $choice in
 	globalTag='START3X_V26A::All'
 	taskName=RawResidualsMC-v$attempt
 	outputFile="rawResidualsMC.root"
+
+	cat $DIR/test/residuals.template.py |sed "s#SET_GLOBALTAG#$globalTag#" |sed "s#SET_OUTPUT#$outputFile#" > pset.$taskName.py
+	cat $DIR/crab/crab.template.cfg |sed "s#SET_DATASET#$dataset#" | sed "s#SET_TASK_NAME#$taskName#" > tmp.cfg
+	cat tmp.cfg| sed "s#SET_PSET#$PWD/pset.$taskName.py#" > crab.$taskName.cfg; rm tmp.cfg
+	crab -create -submit -cfg crab.$taskName.cfg
 	;;
 
     1) echo "choice is 1"
+        ### for MC 
+	attempt=1
+	dataset=/MinBias/Spring10-START3X_V26A_356ReReco-v1/GEN-SIM-RECO 
+	globalTag='START3X_V26A::All'
+	taskName=ResponseAndResolutionsMC-v$attempt
+	outputFile="respAndResoMC.root"
+
+	cat $DIR/test/respAndResoOnMC.template.py |sed "s#SET_GLOBALTAG#$globalTag#" |sed "s#SET_OUTPUT#$outputFile#" > pset.$taskName.py
+	cat $DIR/crab/crab.template.cfg |sed "s#SET_DATASET#$dataset#" | sed "s#SET_TASK_NAME#$taskName#" > tmp.cfg
+	cat tmp.cfg| sed "s#SET_PSET#$PWD/pset.$taskName.py#" > crab.$taskName.cfg; rm tmp.cfg
+	crab -create -submit -cfg crab.$taskName.cfg
 	;;
 
     *) echo "default choice"
@@ -26,7 +42,3 @@ case $choice in
 esac
 
 
-cat $DIR/test/residuals.template.py |sed "s#SET_GLOBALTAG#$globalTag#" |sed "s#SET_OUTPUT#$outputFile#" > pset.$taskName.py
-cat $DIR/crab/crab.template.cfg |sed "s#SET_DATASET#$dataset#" | sed "s#SET_TASK_NAME#$taskName#" > tmp.cfg
-cat tmp.cfg| sed "s#SET_PSET#$PWD/pset.$taskName.py#" > crab.$taskName.cfg; rm tmp.cfg
-crab -create -submit -cfg crab.$taskName.cfg
